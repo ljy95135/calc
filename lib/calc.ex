b2dd02220540c78672c296b2b8fe0a209638f4d2
Jiangyi Lin <lin.jian@husky.neu.edu>
2018-02-04 12:51:04 -0500

    Initial commit

---
+++ lib/calc.ex
@@ -0,0 +1,25 @@
+defmodule Calc do
+  @moduledoc """
+  Documentation for Calc.
+  """
+
+  @doc """
+  main
+
+  ## Examples
+
+      mix run -e Calc.main
+      start the loop
+
+  """
+  def main do
+    IO.gets "test"
+
+    main()
+  end
+
+  @doc """
+
+  """
+  
+end
