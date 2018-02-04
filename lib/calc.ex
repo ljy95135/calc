983b8f692a5e5a3efb6b3134090af147a9c6c01a
Jiangyi Lin <lin.jian@husky.neu.edu>
2018-02-04 15:56:18 -0500

    String to symbol list

--- lib/calc.ex
+++ lib/calc.ex
@@ -10,16 +10,75 @@ defmodule Calc do
 
       mix run -e Calc.main
       start the loop
-
   """
-  def main do
-    IO.gets "test"
-
+  def main() do
+    eval(IO.gets(""))
     main()
   end
 
   @doc """
-
+  eval
+  caculate the result and puts at screen.
   """
-  
+  def eval(str) do
+    charList = String.graphemes(str)
+    IO.inspect(charList)
+    calcList = strToList(charList, [])
+    IO.inspect(calcList)
+    listToResult(calcList,[],[])
+  end
+
+  @doc """
+  listToResult
+  From infix list to result
+  """
+  def listToResult(calc, numStack, opStack) do
+    cond do
+
+    end
+  end
+
+  @doc """
+  strToList
+  From string to number-operator List
+  """
+  def strToList(chars, result) do
+    cond do
+      # return condition
+      List.first(chars) == "\n" ->
+        result
+
+      # operator
+      List.first(chars) == "+" or List.first(chars) == "-"
+      or List.first(chars) == "*" or List.first(chars) == "/"
+      or List.first(chars) == "(" or List.first(chars) == ")" ->
+        strToList(getTail(chars), result ++ [List.first(chars)])
+
+      # space
+      List.first(chars) == " " ->
+        strToList(getTail(chars), result)
+
+      # number, if not, will raise a error.
+      true -> getNumber(chars, result)
+    end
+  end
+
+  @doc """
+  getNumber
+  Use Integer.parse to get the number
+  """
+  def getNumber(l, result) do
+    tup = Integer.parse(Enum.join(l))
+    chars = String.graphemes(elem(tup, 1))
+    strToList(chars, result ++ [elem(tup, 0)])
+  end
+
+  @doc """
+  getTail
+  Get the tail of a List.
+  """
+  def getTail(l) do
+    [_ | tail] = l
+    tail
+  end
 end
--- test/calc_test.exs
+++ test/calc_test.exs
@@ -2,7 +2,7 @@ defmodule CalcTest do
   use ExUnit.Case
   doctest Calc
 
-  test "greets the world" do
-    assert Calc.hello() == :world
-  end
+#  test "greets the world" do
+#    assert true
+#  end
 end
